2a1ba1367e297d0b6bd71b8cbddb3d6fc044b531
Gaurav <Gaurav@Gaurav>
2025-06-15 12:54:43 +0530

    Home Network

--- GSProject/src/test/java/stepDefs/RegModule/GetAlertRegSD.java
+++ GSProject/src/test/java/stepDefs/RegModule/GetAlertRegSD.java
@@ -1,5 +1,7 @@
 package stepDefs.RegModule;
 
+import java.util.HashMap;
+
 import io.cucumber.java.en.*;
 import utils.GSTProjectUtils;
 import utils.Heart;
@@ -9,6 +11,7 @@ public class GetAlertRegSD {
 	Heart heart;
 	GSTProjectUtils gstpu;
 	String govtAuthToken;
+	HashMap<String, String> alertMap = new HashMap<>();
 	public GetAlertRegSD() {
 		this.heart = Heart.returnHeartObject();
 	}
@@ -20,13 +23,17 @@ public class GetAlertRegSD {
 		System.out.println("Govt Auth token issss: "+ govtAuthToken);
 	}
 
-	@Given("the user has the url")
-	public void the_user_has_the_url() {
+	@Given("^the user create alert parameter list with (.+), (.+), (.+), (.+)$")
+	public void create_parameter_list(String state_cd, String start_tm, String end_tm, String action) {
+		alertMap.put("state_cd", state_cd);
+		alertMap.put("start_tm", start_tm);
+		alertMap.put("end_tm", end_tm);
+		alertMap.put("action", action);
 	}
 
 	@When("the user hits GetAlertAPI")
 	public void the_user_hits_get_alert_api() {
-		heart.response = heart.blood.getGetAlertRegOpsObject().getRegAlertResponse(govtAuthToken, getAlertPath, heart);
+		heart.response = heart.blood.getGetAlertRegOpsObject().getRegAlertResponse(govtAuthToken, alertMap, getAlertPath, heart);
 		System.out.println("In SD");
 	}
 
